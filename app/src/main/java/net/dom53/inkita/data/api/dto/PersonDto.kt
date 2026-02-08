5dd40ee05d5545947dac6cb423fe38f8ff2858d1
Michael Huyler <michaelhuyler@gmail.com>
2026-02-08 00:19:38 -0600

    Tighten up typing of Person DTO

--- app/src/main/java/net/dom53/inkita/data/api/dto/PersonDto.kt
+++ app/src/main/java/net/dom53/inkita/data/api/dto/PersonDto.kt
@@ -1,13 +1,13 @@
 package net.dom53.inkita.data.api.dto
 
 data class PersonDto(
-    val id: Int? = null,
-    val name: String? = null,
+    val id: Int = 0,
+    val name: String = "",
     val coverImageLocked: Boolean? = null,
     val primaryColor: String? = null,
     val secondaryColor: String? = null,
     val coverImage: String? = null,
-    val aliases: List<String>? = null,
+    val aliases: List<String> = emptyList(),
     val description: String? = null,
     val asin: String? = null,
     val aniListId: Int? = null,
